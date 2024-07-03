1835938d0a8d0b47d9cb89e9a0ee00ad873dc11e
Felix Waiblinger <f.waiblinger@tum.de>
2024-07-03 11:52:08 +0200

    added track and drone check

--- lsy_drone_racing/utils/__init__.py
+++ lsy_drone_racing/utils/__init__.py
@@ -8,6 +8,8 @@ from lsy_drone_racing.utils.utils import (
     draw_trajectory,
     load_config,
     load_controller,
+    check_drone_start_pos,
+    check_race_track
 )
 
-__all__ = ["load_config", "load_controller", "check_gate_pass", "draw_trajectory"]
+__all__ = ["load_config", "load_controller", "check_gate_pass", "draw_trajectory", "check_drone_start_pos", "check_race_track"]
